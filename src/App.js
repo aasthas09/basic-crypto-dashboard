b1db86c40d55bb4ae0614e38dbfcc1eca9fa4159
aasthas09 <singhaastha009@gmail.com>
2021-08-16 22:31:27 +0530

    First Hook

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef } from "react";
+import Dashboard from "./components/Dashboard";
+import { formatData } from "./utils";
+import "./styles.css";
+
+export default function App() {
+  const [currencies, setcurrencies] = useState([]);
+  const [pair, setpair] = useState("");
+  const [price, setprice] = useState("0.00");
+  const [pastData, setpastData] = useState({});
+  const ws = useRef(null);
+
+  let first = useRef(false);
+  const url = "https://api.pro.coinbase.com";
+
+  useEffect(() => {
+    ws.current = new WebSocket("wss://ws-feed.pro.coinbase.com");
+
+    let pairs = [];
+
+    const apiCall = async () => {
+      await fetch(url + "/products")
+        .then((res) => res.json())
+        .then((data) => (pairs = data));
+      
+      let filtered = pairs.filter((pair) => {
+        if (pair.quote_currency === "USD") {
+          return pair;
+        }
+      });
+
+      filtered = filtered.sort((a, b) => {
+        if (a.base_currency < b.base_currency) {
+          return -1;
+        }
+        if (a.base_currency > b.base_currency) {
+          return 1;
+        }
+        return 0;
+      });
+
+      
+      setcurrencies(filtered);
+
+      first.current = true;
+    };
+
+    apiCall();
+  }, []);
+
+}
